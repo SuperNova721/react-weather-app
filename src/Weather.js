b5e919bdf1e4de881d6a5bb2fd92988c3f4bfc70
SuperNova721 <94159279+SuperNova721@users.noreply.github.com>
2022-03-25 22:39:15 -0700

    Added css in both Weather and App css files. Created a search bar added weather infomation

--- src/Weather.js
+++ src/Weather.js
@@ -6,10 +6,30 @@ export default function Weather() {
     <div className="weather">
       <div className="current-weather">
         <h2>London</h2>
-        <ul>
-          <li>Wednesday 4:30</li>
-          <li>Description: Cloudy</li>
-        </ul>
+        <div className="row m-0">
+          <div className="weather-image-info col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Sunny"
+            />
+            62°F
+          </div>
+          <div className="weather-info col-6">
+            <ul>
+              <li>Wednesday 4:30</li>
+              <li>Description: Cloudy</li>
+            </ul>
+          </div>
+          <p className="revolving-weather-info"> Revolving weather info</p>
+        </div>
+        <form>
+          <input
+            className="search-bar"
+            type="search"
+            placeholder="Enter City Name..."
+          />
+          <input className="search-button" type="submit" value="Search" />
+        </form>
       </div>
     </div>
   );
