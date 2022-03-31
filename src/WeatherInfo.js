72ade31d969467c29e59a76e8e80ace58de35532
SuperNova721 <94159279+SuperNova721@users.noreply.github.com>
2022-03-30 22:13:21 -0700

    Moved css from weather file to weatherinfo.css file. Added additional css

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate.js";
+import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
   return (
@@ -8,18 +9,36 @@ export default function WeatherInfo(props) {
         <h2>{props.data.city}</h2>
         <div className="row m-0">
           <div className="weather-image-info col-6">
-            <img src={props.data.iconURL} alt={props.data.description} />
-            {Math.round(props.data.temperature)}°F
+            <img
+              className="weather-icon w-50"
+              src={props.data.iconURL}
+              alt={props.data.description}
+            />
+            <span className="current-temperature">
+              {Math.round(props.data.temperature)}
+            </span>
+            <span className="units">
+              <span className="metric">°C|</span>
+              <span className="fehrenheit">
+                <a href="btn" target="_blank">
+                  °F
+                </a>
+              </span>
+            </span>{" "}
           </div>
-          <div className="weather-info col-6">
+          <div className="weather-status col-6">
             <ul>
               <li>
                 <FormattedDate date={props.data.date} />
               </li>
               <li className="text-capitalize">{props.data.description}</li>
+              <li>
+                <p className="revolving-weather-info text-capitalize">
+                  Revolving weather info
+                </p>
+              </li>
             </ul>
           </div>
-          <p className="revolving-weather-info"> Revolving weather info</p>
         </div>
       </div>
     </div>
