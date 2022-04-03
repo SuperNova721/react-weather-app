9fc2e2405f8934ed4b6dd281f1566c92f25eb5a6
SuperNova721 <94159279+SuperNova721@users.noreply.github.com>
2022-04-03 16:45:19 -0700

    tracked the current unit of the component based on temperature in celsius or fahrenheit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+
+  }
+
+  if (unit === "celsius") {
   return (
     <div className="weather-temperature" inline>
       <span className="current-temperature">{Math.round(props.celsius)}</span>
       <span className="units">
-        <span className="metric">°C|</span>
-        <span className="fehrenheit">
-          <a href="btn" target="_blank">
+        °C|{" "}
+        <a href="/" onClick={showFahrenheit}>
             °F
           </a>
         </span>
-      </span>
+      
     </div>
   );
+} else {
+  let fahrenheit = (props.celsius * 9) / 5 + 32; 
+  return (
+    <div className="weather-temperature">
+      <span className="current-temperature">{Math.round(fahrenheit)}</span>
+<span className="units">
+  <a href="/" onClick={showCelsius}>°C</a>{" "}
+  |°F
+</span>
+    </div>
+  );
+}
 }
