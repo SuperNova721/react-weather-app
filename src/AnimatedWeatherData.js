440788b38342d11663627644bb60c12467ec4794
SuperNova721 <94159279+SuperNova721@users.noreply.github.com>
2022-04-10 22:17:10 -0700

    Removed unneeded variable

--- src/AnimatedWeatherData.js
+++ src/AnimatedWeatherData.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./AnimatedWeatherData.css";
 
 export default function AnimatedWeatherData(props) {
-  let windSpeed = `WindSpeed: ${Math.round(props.data.windSpeed)}`;
-
   return (
     <div className="AnimatedWeatherData">
       <span className="data-first">Wind: {Math.round(props.data)}mph</span>
